3e7525c95831897e5dc18d7fb1793f4bbe0bde7a
diegodocampo <diegodocampo1@gmail.com>
2026-08-30 13:12:22 +0200

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,15 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX';
+  
+  script.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-XXXXXXXXXX');
+  };
+  
+  document.head.appendChild(script);
+})();
