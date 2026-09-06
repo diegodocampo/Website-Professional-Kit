6020d7d79635126d2496cc20aae8757ecc8fc8f0
diegodocampo <diegodocampo1@gmail.com>
2026-09-06 10:08:51 +0100

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,14 +1,14 @@
 (function() {
   var script = document.createElement('script');
   script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX';
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-L5BKFJ3K5E';
   
   script.onload = function() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){ dataLayer.push(arguments); }
     window.gtag = gtag;
     gtag('js', new Date());
-    gtag('config', 'G-XXXXXXXXXX');
+    gtag('config', 'G-L5BKFJ3K5E');
   };
   
   document.head.appendChild(script);
